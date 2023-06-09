22424546b9785f15ecf325b0e84fd4e36ab3afb5
David Lawrence <davidl@jlab.org>
2023-06-09 18:04:07 -0400

    Make gluon links script

---
+++ make_gluon_links.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+# This script generates useful links (to DATA, ROOT and other directories) for GLUON machines
+ln -s /home/hdtrdops/DATA DATA
+ln -s /gluonraid3/data4/rawdata/trd/LOG LOG
+ln -s /home/hdtrdops/ROOT ROOT
+ln -s /home/hdtrdops/soft_ml4fpga/setup_env.csh setup_env.csh
+ln -s /home/hdtrdops/soft_ml4fpga/setup_env.sh setup_env.sh
